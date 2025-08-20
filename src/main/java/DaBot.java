45e79cdb8dcae49cff661045e7824bdb4a992c80
Damian Liew Cho Xiang <damiancxliew@gmail.com>
2025-08-20 15:34:42 +0800

    Implement Level-0: renamed main class and added greeting

---
+++ src/main/java/DaBot.java
@@ -0,0 +1,10 @@
+public class DaBot {
+    public static void main(String[] args) {
+        System.out.println("____________________________________________________________\n");
+        System.out.println("Hello! I'm DaBot\n");
+        System.out.println("What can I do for you?\n");
+        System.out.println("____________________________________________________________\n");
+        System.out.println("Bye. Hope to see you again soon!\n");
+        System.out.println("____________________________________________________________\n");
+    }
+}
