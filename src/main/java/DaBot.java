e0ab2e955a8bf60a7347bd281a4606ce07c46478
Damian Liew Cho Xiang <damiancxliew@gmail.com>
2025-08-21 12:00:41 +0800

    =Implement Level-1: added echo

--- src/main/java/DaBot.java
+++ src/main/java/DaBot.java
@@ -1,10 +1,24 @@
+import java.util.Scanner;
+
 public class DaBot {
+    public static boolean echo(String word) {
+        if (word.contains("bye")) {
+            System.out.println("Bye. Hope to see you again soon!\n");
+            return false;
+        }
+        System.out.println(word);
+        return true;
+    }
     public static void main(String[] args) {
-        System.out.println("____________________________________________________________\n");
         System.out.println("Hello! I'm DaBot\n");
         System.out.println("What can I do for you?\n");
-        System.out.println("____________________________________________________________\n");
-        System.out.println("Bye. Hope to see you again soon!\n");
-        System.out.println("____________________________________________________________\n");
+        Scanner scanner = new Scanner(System.in);
+
+        while (true) {
+            String word = scanner.next();
+            if (!echo(word)) {
+                break;
+            }
+        }
     }
 }
